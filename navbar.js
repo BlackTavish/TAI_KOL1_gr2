be1f1c873f3a784675d2247fdef87d4fa0953bb1
BlackTavish <kubalos98@gmail.com>
2021-04-09 15:35:21 +0200

    Final commit

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<nav><ul> <li><a href="index.html">Home  </a></li>' + 
+'<li><a href="kontakt.html">Kontakt</a></li>' + 
+'<li><a href="formularz.html">Formularz</a></li></ul </nav>';
+
+let navbarElem = document.querySelector(".nav-bar"); 
+
+navbarElem.innerHTML = nav;
---
+++ zad2.js
@@ -0,0 +1,3 @@
+function changeBackground(){
+    document.body.style.backgroundColor = "#4C4C4C";
+}
